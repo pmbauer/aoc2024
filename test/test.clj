d0b3cd372d51e41970fc53302cb686d5a6500bbf
Paul Bauer <paul@bauer.codes>
2024-12-08 08:59:50 -0500

    squash

--- test/test.clj
+++ test/test.clj
@@ -4,7 +4,12 @@
    [hyperfiddle.rcf :as rcf]))
 
 (def default-under-test
-  '(codes.bauer.aoc2024.01))
+  '(codes.bauer.aoc2024.02
+    codes.bauer.aoc2024.03
+    codes.bauer.aoc2024.04
+    codes.bauer.aoc2024.05
+    codes.bauer.aoc2024.06
+    codes.bauer.aoc2024.07))
 
 (defn run [{:keys [namespaces]
             :or {namespaces default-under-test}}]
